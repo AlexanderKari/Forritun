322a3313fe1c7fc36c0ebd93cce50d46721e152f
AlexanderKari <alexanderra222@nemi.tskoli.is>
2017-01-25 14:18:09 +0000

    Dæmi 2 lokið

--- git_forritun.py
+++ git_forritun.py
@@ -5,3 +5,8 @@ utk1=tala1+tala2
 utk2=tala1*tala2
 print("Tölurnar lagðar saman: ",utk1)
 print("Tölurnar lagðar saman: ",utk2)
+
+#Liður 2
+fornafn=input("Slaðu inn fornafn ")
+eftirnafn=input("Slaðu inn eftirnafn ")
+print("Halló",fornafn,eftirnafn)
