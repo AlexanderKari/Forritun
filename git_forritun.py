231ee5ae3360fa0b75d4c67b01464d0524bd6c71
AlexanderKari <alexanderra222@nemi.tskoli.is>
2017-01-25 14:14:46 +0000

    Dæmi eitt lokið

---
+++ git_forritun.py
@@ -0,0 +1,7 @@
+#Liður 1
+tala1=int(input("Slaðu inn tölu eitt "))
+tala2=int(input("Slaðu inn tölu tvö "))
+utk1=tala1+tala2
+utk2=tala1*tala2
+print("Tölurnar lagðar saman: ",utk1)
+print("Tölurnar lagðar saman: ",utk2)
